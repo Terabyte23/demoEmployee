65bb3fed8e581b22fcdcc0655c39e48b1031d523
user <kristiina.parri@ivkhk.ee>
2024-09-11 11:20:15 +0300

    first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class App {
+    public void run(){
+        Address address = new Address();
+        address.setCity("Narva");
+        address.setState("Ida-Viiruma");
+        address.setZip("41536");
+        address.setStreet("Tallinna mnt.");
+        address.setHouse("80");
+        address.setRoom("20");
+        Person person = new Person("Ivan","Ivanov",10, 10, 2000, address);
+        Employee employee = new Employee(person, "Director", "3000");
+
+    }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,86 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Person {
+    private String name;
+    private String surname;
+    private int birthYear;
+    private int birthMonth;
+    private int birthDay;
+    private Address address;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getBirthMonth() {
+        return birthMonth;
+    }
+
+    public void setBirthMonth(int birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public int getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(int birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return birthYear == person.birthYear && birthMonth == person.birthMonth && birthDay == person.birthDay && Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, birthYear, birthMonth, birthDay, address);
+    }
+
+    public Person(String name, String surname, int birthYear, int birthMonth, int birthDay, Address address) {
+        this.name = name;
+        this.surname = surname;
+        this.birthYear = birthYear;
+        this.birthMonth = birthMonth;
+        this.birthDay = birthDay;
+        this.address = address;
+
+    }
 }
